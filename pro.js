b19f8f881ad815896a31b750bf47ae0dce9920e5
funeyu <funeyu@gmail.com>
2017-04-10 17:46:36 +0800

    异步调用使用MutationObserver

--- pro.js
+++ pro.js
@@ -1,24 +1,30 @@
+
 var FuPromise = function(excutor){
   if(typeof excutor !== 'function') {
     return new Error('excutor should be a function')
   }
-  this.follow;
-  this.state = 'PENDING';
-  this.result;
-  this.thens = [];
+  this.follow
+  this.state = 'PENDING'
+  this.result
+  this.thens = []
+
   this._execute(excutor)
+}
+
+FuPromise.prototype.followee = function() {
+  return this.follow
 }
 
 FuPromise.prototype._execute = function(excutor) {
 
-  excutor.call(this, this.onFulfilled.bind(this), this.onRejected)
+  excutor.call(this, this.onFulfilled.bind(this), this.onRejected.bind(this))
 }
 
 FuPromise.prototype.onFulfilled = function(value) {
   if(this.state !== 'PENDING') return
 
   this.state = 'FULFILLED'
-  this.result = value;
+  this.result = value
   this._async(this, void 0, void 0)
 }
 
@@ -26,19 +32,34 @@ FuPromise.prototype.onRejected = function(reason) {
   if(this.state !== 'PENDING') return
 
   this.state = 'REJECTED'
-  this.result = reason;
+  this.result = reason
+  this._async(this, void 0, void 0)
 }
 
 FuPromise.prototype._async = function(promise, handler, arg) {
   var me = this
+  console.log(me)
+  // if(!me.followee()) return
 
-  setTimeout(function() {
-
+  FuPromise.schedule(function() {
     for(var i = 0; i < me.thens.length; i ++) {
-      console.log('result', me)
-      me.thens[i].fulfillmentHandler.call(me, me.result)
+      var followee = me.thens[i].promise
+      if(me.state === 'FULFILLED') {
+        try {
+          var result = me.thens[i].fulfillmentHandler.call(me, me.result)
+          if(result && typeof result.then !== 'undefined') {
+            // 如果then返回的是promise就直接将后继promise的thens 接到此promise上
+            result.thens = followee.thens
+          }
+          else {
+            followee.onFulfilled(result)
+          }
+        } catch(error) {
+
+        }
+      }
     }
-  }, 0)
+  })
 }
 
 FuPromise.prototype.then = function(onFulfilled, onRejected) {
@@ -46,13 +67,49 @@ FuPromise.prototype.then = function(onFulfilled, onRejected) {
   this.follow = promise
   var me = this
   this.thens.push({
+    promise: promise,
     fulfillmentHandler: function(result) {
-      onFulfilled(result)
+      return onFulfilled(result)
     },
     rejectionHandler: function(reason) {
-      onRejected(reason)
+      return onRejected(reason)
     }
   })
 
   return promise
+}
+
+// 根据浏览器环境设置异步调用
+if(typeof MutationObserver !== 'undefined' && typeof window !== 'undefined') {
+  FuPromise.schedule = (function() {
+    var div = document.createElement('div')
+    var opts = {attributes: true}
+    var div2 = document.createElement('div')
+    toggleScheduled = false
+    var o2 = new MutationObserver(function() {
+      div.classList.toggle('foo')
+      toggleScheduled = false
+    })
+    o2.observe(div2, opts)
+
+    var scheduleToggle = function() {
+      if(toggleScheduled) return
+      toggleScheduled = true
+      div2.classList.toggle('foo')
+    }
+
+    return function schedule(fn) {
+      var o = new MutationObserver(function() {
+        o.disconnect()
+        fn()
+      })
+      o.observe(div, opts)
+      scheduleToggle()
+    }
+  })()
+}
+else if(typeof setTimeout !== 'undefined') {
+  FuPromise.schedule = function(fn) {
+    setTimeout(fn, 0)
+  }
 }
